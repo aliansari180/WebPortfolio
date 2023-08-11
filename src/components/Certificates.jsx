903c1987cb7c4923bb5582c2c4d99c673a5939f2
aliansari180 <muhammadaliansari180@gmail.com>
2023-08-11 15:30:22 +0500

    final changes

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -43,33 +43,52 @@ function Certificates() {
     <img src="digital.jpg" alt="certificate" />
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/L4G53FBT2334">
+    <img src="intro.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/78CM6BGKNG2V">
+    <img src="introjs.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/78SZQH3UVGUB">
+    <img src="htmlcss.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/6983ZLP6FTWX">
+    <img src="uiux.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/8KH2KYT5DJPS">
+    <img src="reactbasic.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/RQ25KRDPLVDL">
+    <img src="version.jpg" alt="certificate" />
+    </a>
   </div>
   <div>
-    <img src="digital.jpg" alt="certificate" />
+    <a href="https://www.coursera.org/account/accomplishments/verify/CBHTYNAN877D">
+    <img src="advancedreact.jpg" alt="certificate" />
+    </a>
+  </div>
+  <div>
+    <a href="https://www.coursera.org/account/accomplishments/verify/KBG5PA7YZ83E">
+    <img src="capstone.jpg" alt="certificate" />
+    </a>
+  </div>
+  <div>
+    <a href="https://www.coursera.org/account/accomplishments/verify/HJ6MEKKZ6SUB">
+    <img src="interview.jpg" alt="certificate" />
+    </a>
   </div>
 </div>
-
-
-
-
-       
-        </>
+  </>
   );
 }
 
